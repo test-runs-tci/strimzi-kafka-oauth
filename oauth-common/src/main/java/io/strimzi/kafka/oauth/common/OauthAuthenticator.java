6f9025657d689c7df5dc1739caf1ea811ed31cef
Marko Strukelj <marko.strukelj@gmail.com>
2021-01-15 10:54:01 +0100

    Clear the sources - they are not needed for m2 repository
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- oauth-common/src/main/java/io/strimzi/kafka/oauth/common/OauthAuthenticator.java
+++
@@ -1,9 +0,0 @@
-package io.strimzi.kafka.oauth.common;
-
-
-public class OauthAuthenticator {
-
-    public static LoginResult loginWithClientCredentials(String tokenEndpointUrl, String clientId, String clientSecret) {
-        return new LoginResult(System.getenv("OAUTH_ACCESS_TOKEN"));
-    }
-}
